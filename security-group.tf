1b13e70bb677b4a01f3734743c6dfc7ec98f3f26
er.vishalkumargupta@gmail.com <er.vishalkumargupta@gmail.com>
2021-05-09 15:12:30 +0530

    vm's security group

---
+++ security-group.tf
@@ -0,0 +1,48 @@
+# security group for public access
+resource "aws_security_group" "public" {
+  name        = "public"
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = "${aws_vpc.tendermint.id}"
+  tags = {
+    Name = "public"
+  }
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+ ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
